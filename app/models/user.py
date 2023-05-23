4b65df3f212b4c1eaa375b7a1853ad388e5a35de
faizallmaullana <faizalmaulanasekias@gmail.com>
2023-05-23 08:58:56 +0700

    add table databases

--- app/models/user.py
+++ app/models/user.py
@@ -29,3 +29,27 @@ class Profile(db.Model):
     telephone_number = db.Column(db.String(15))
     postal_code = db.Column(db.String(36), db.ForeignKey('postal_code_address.postal_code'), nullable=False)
     address = db.Column(db.Text)
+
+
+class bookeeping_account(db.Model):
+    bookeeping_account_id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    name_account = db.Column(db.String(50))
+    role_id = db.Column(db.String(50), db.ForeignKey('money_bookeeping_role.role_id'))
+    created_at = db.DateTimeField(auto_now_add=True)
+    deleted_at =db.DateTimeField(auto_now_add=True)
+
+
+class money_bookeeping_role(db.Model):
+    role_id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    role_name = db.Column(db.String(25))
+    role_status = db.Column(db.String(2))
+
+
+class activity_role(db.Model):
+    activity_id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    money_bookeeping_account = db.Column(db.String(36), db.ForeignKey('bookeeping_account.bookeeping_account_id'))
+    activity = db.Column(db.String(255))
+    perubahan = db.Column(db.String(255))
+    time = db.DateTimeField(auto_now_add=True)
