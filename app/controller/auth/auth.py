884a835643ff4ddcebe587f04c2326cf37ce5428
faizallmaullana <faizalmaulanasekias@gmail.com>
2023-06-01 21:57:16 +0700

    solving register

--- app/controller/auth/auth.py
+++ app/controller/auth/auth.py
@@ -2,6 +2,7 @@ from flask import request, abort, jsonify, current_app
 from flask_restful import Resource, reqparse, fields, marshal_with
 from flask.config import Config
 
+from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user
 
@@ -48,30 +49,47 @@ class Login(Resource):
 
     
 # Bikin logika register di sini
-
 class Register(Resource):
     def post(self):
-        return jsonify({"tes":"tes"})
-        # Ambil data dari permintaan POST
-        username = request.form.get('username')
-        password = generate_password_hash(request.form.get('password'))
+            parser = reqparse.RequestParser()
+            parser.add_argument('username', type=str, required=True)
+            parser.add_argument('password', type=str, required=True)
+            args = parser.parse_args()
 
-        # validasi data
-        if not username or not password:
-            return jsonify({
-                "Pesan": "Username dan password harus diisi",
-                "Status": 400
-            })
+            users = Users(
+                # user_id=args['user_id'],
+                username=args['username'],
+                password=generate_password_hash(args['password']),
+            )
 
-        # Simpan data ke database
-        user = Users(username=username, password=password)
-        user.save()  
+            db.session.add(users)
+            db.session.commit()
 
-        # Return JSON response
-        return jsonify({
-            "Data": {
-                "Username": username
-            },
-            "Pesan": "Registrasi berhasil",
-            "Status": 200
-        })
+            return {'message': 'Profile created successfully'}, 201
+
+# class Register(Resource):
+#     def post(self):
+#         # Ambil data dari permintaan POST
+#         username = request.form.get('username')
+#         password = request.form.get('password') # hashing password nya dihapus dulu
+
+#         # validasi data
+#         if not username or not password:
+#             return jsonify({
+#                 "Pesan": "Username dan password harus diisi",
+#                 "Status": 400
+#             })
+
+#         # Simpan data ke database
+#         user = Users(username=username, password=password)
+#         user.save()  
+
+#         # Return JSON response
+#         return jsonify({
+#             "Data": {
+#                 "Username": username
+#             },
+#             "Pesan": "Registrasi berhasil",
+#             "Status": 200
+#         })
+
