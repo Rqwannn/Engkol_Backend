0124cd28eec4ef508ecf0daa3cb43849ca571216
faizallmaullana <faizalmaulanasekias@gmail.com>
2023-07-04 08:57:47 +0700

    end point bussiness plan

--- app/path_url/bussines_plan.py
+++ app/path_url/bussines_plan.py
@@ -3,4 +3,4 @@ from app.controller.bussines_plan.main import *
 
 
 def business_plan_api_path():
-    api.add_resource(OpenAIApi, "/openAI")
+    api.add_resource(OpenAIApi, "/api/v1/engkol/resource/register/bussiness_plan")
